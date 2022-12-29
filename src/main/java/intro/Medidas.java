4ed3cfcc181f9668d7091aa40e0224b3bf0a46bd
Edivania Silva <edivania.silva@sensedia.com>
2022-12-29 15:14:30 -0300

    Inclusão de menu de opções e contagem crescente e decrescente com for.

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -3,6 +3,8 @@ package intro;
 
 // 2 - Referencia as Bibliotecas
 
+import java.util.Scanner;
+
 // 3 - Classe
 public class Medidas {
     // 3.1 - Atributos/Características
@@ -10,9 +12,65 @@ public class Medidas {
     // 3.2 - Métodos e Funções
     public static void main (String[] args){
 
-        //condicional = verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou software
-        calcularAreaModoCompacto();
-        calcularAreaModoExtenso();
+        //condicional = verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou software.
+
+        // Utilizar a classe Scanner do java para receber a esolha do usuário no console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("M E N U   D E   O P Ç Õ E S: ");
+        System.out.println("C - Calcular Área Modo Compacto ");
+        System.out.println("D - Contar Até Dez");
+        System.out.println("E - Calcular Área Modo Extenso ");
+        System.out.println("I - If Simples ");
+        System.out.println("R - Contagem Regressiva de 10 a 0");
+        System.out.println("Digite a opção desejada: ");
+        String opcao = scanner.next();
+
+        //Switch = seletor (selecionar o comportamento de acordo com a escolha da pessoa.
+
+        switch (opcao){
+            case "c":
+            case "C":
+                System.out.println("Voce selecionou o compacto");
+                calcularAreaModoCompacto();
+            case "d":
+            case "D":
+                System.out.println("Voce selecionou contar até 10");
+                contarAteDez();
+                break;
+            case "e":
+            case "E":
+                System.out.println("Voce selecionou o extenso");
+                calcularAreaModoExtenso();
+                break;
+            case "i":
+            case "I":
+                System.out.println("Voce selecionou if");
+                ifSimples();
+                break;
+            case "r":
+            case "R":
+                System.out.println("Voce selecionou contagem regressiva");
+                contagemRegressiva();
+                break;
+            default:
+                System.out.println("Nenhuma opção selecionada");
+        }
+    }
+
+    public static void ifSimples(){
+        // If/Else
+        String modo = "Banana";
+
+        if (modo == "compacto"){
+            calcularAreaModoCompacto();
+        }
+        if (modo == "extenso"){
+            calcularAreaModoExtenso();
+        }
+        else{
+            System.out.println("Somente as opções 'Compacto' e 'Extenso' deve ser informada.");
+        }
+
     }
 
     public static void calcularAreaModoExtenso(){
@@ -40,5 +98,21 @@ public class Medidas {
         int comprimento = 3;
 
         System.out.println("Para a largura de " + largura + "m e o comprimento de " + comprimento + "m, a área é de " + largura * comprimento + " m2.");
+    }
+
+    public static void contarAteDez(){
+        // For
+        System.out.println("Contagem Crescente");
+        for(int numero = 1; numero < 11; numero++){
+            System.out.println("Numero: " + numero);
+        }
+    }
+
+    public static void contagemRegressiva(){
+        // For
+        System.out.println("Contagem Regressiva");
+        for(int numero = 10; numero > -1; numero--){
+            System.out.println("Numero: " + numero);
+        }
     }
 }
